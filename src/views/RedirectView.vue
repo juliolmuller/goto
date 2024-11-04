be721293f49a12a3830974e7c1e53891c40f5ae2
Julio Muller <julio.muller@rdstation.com>
2024-11-04 09:57:53 -0300

    feat: add border

--- src/views/RedirectView.vue
+++ src/views/RedirectView.vue
@@ -26,6 +26,7 @@ defineProps<RedirectViewProps>();
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  border-left: 0 solid var(--color-border);
 }
 
 h1 {
@@ -49,6 +50,12 @@ a {
 
   &:hover {
     border-color: transparent;
+  }
+}
+
+@media (min-width: 1024px) {
+  .redirect {
+    border-left-width: 1px;
   }
 }
 </style>
